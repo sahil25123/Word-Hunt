0b499bcffcffc904abbfbe129467b6bf8b322512
sahil25123 <sahilgupta25123@gmail.com>
2025-05-10 02:03:40 +0530

    Refactor styles in App.css and implement Header component layout with input field

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,17 @@
+import { createTheme, TextField, ThemeProvider } from '@mui/material'
 import React from 'react'
 
 const Header = () => {
+    
+
   return (
     <div className='header'>
         <span className='title'>Word Hunt</span>
+        <div className='inputs'>
+        
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+           
+        </div>
       
     </div>
   )
