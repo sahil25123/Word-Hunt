035bf9632647ec763b2869dacaf5e6f5ec490c79
sahil25123 <sahilgupta25123@gmail.com>
2025-05-10 01:27:20 +0530

    Initialize Word Hunt project with Vite, React, and MUI; add basic dictionary functionality and styles

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+
+
+import axios from 'axios'
+import './App.css'
+import { useEffect, useState } from 'react';
+import { Container } from '@mui/material';
+
+function App() {
+  const [word, setWord] = useState("");
+  const [meanings , setMeanings] = useState([]);
+
+  const dic = async() =>{
+    try{
+      const res = await axios.get("https://api.dictionaryapi.dev/api/v2/entries/en/boat")
+      console.log(res.data);
+      setMeanings(res.data)
+
+    }
+    catch(e){
+      console.log(e)
+    
+  }
+}
+console.log(meanings)
+useEffect(() =>{
+  dic();
+
+},[])
+  
+
+  return (
+    <>
+      
+      <div className='app'>
+        <Container maxWidth="lg">
+          Dictonary
+
+          
+        </Container>
+
+      </div>
+    </>
+  )
+}
+
+export default App
